38438766d0fd753a19b1b9495bcc88e7b9848588
waq-e <whomomlikes@hotmail.com>
2022-03-12 00:17:55 -0600

    Initial commit.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	// Three ways of declaring and initializing vectors.
+	vector<int> v_1{0, 1, 2};
+	vector<int> v_2 = {3, 4, 5};
+	vector<int> v_3;
+	v_3 = {6};
+	cout << "Everything worked!" << "\n";
 }
