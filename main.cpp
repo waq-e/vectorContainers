aeb86470890b45053170b18753bf3c61d1160035
waq-e <whomomlikes@hotmail.com>
2022-03-12 00:23:02 -0600

    2D Vector added.
    Initial commit.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <vector>
-using std::vector;
-using std::cout;
 
-// Vectors are a sequence of elements of a single type, and have useful methods for
-// getting the size, testing if the vector is empty, and adding elements to the vector.
+using std::cout;
+using std::vector;
+
+// Vectors are a sequence of elements of a single type, and have useful methods
+// for getting the size, testing if the vector is empty, and adding elements to
+// the vector.
 int main() {
 	// Three ways of declaring and initializing vectors.
-	vector<int> v_1{0, 1, 2};
-	vector<int> v_2 = {3, 4, 5};
+	vector<int> v_1{0, 1, 2};    // initialization list
+	vector<int> v_2 = {3, 4, 5}; // initialization list
 	vector<int> v_3;
 	v_3 = {6};
-	cout << "Everything worked!" << "\n";
+	cout << "Everything worked!"
+	     << "\n";
+
+	// Creating a 2D vector.
+	vector<vector<int>> v{{1, 2},
+	                      {3, 4}};
+	cout << "Great! A 2D vector has been created." << "\n";
 }
